29e4cde67d538eef8684182d56fd4bad6c9e8631
MiYeon Lee <miyeonlee0618@gmail.com>
2023-06-02 17:58:54 +0900

    last commit

--- src/comp/Hd.js
+++ src/comp/Hd.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-class To extends Component {render (){return (<header>
-  <h1>{this.props.name}</h1>Hello, {this.props.tx}</header> )}}
-  
+class To extends Component {render (){console.log('Header render');
+return (<header>
+  <h1><a href="/" onClick={(p)=>{p.preventDefault();
+  this.props.ch()}}>{this.props.name}</a></h1>{this.props.tx}</header>)}}  
   export default To;
